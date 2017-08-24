1b6f0ae4e5889036eaf79b1b5b5985b0120a48ba
Gabisonfire <gab.despres@gmail.com>
2017-08-23 22:05:48 -0400

    Assembly name changes

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Steam_Dock.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Steam_Game_Launcher.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Steam_Dock.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
